fa8ed44b7f4ff9c91199c9c63dcce6dce0ea7414
Greg <gmerrill03@gmail.com>
2019-11-06 16:36:57 -0800

    Add refactored solution for toweirdcase

--- codewars/to-weird-case.js
+++ codewars/to-weird-case.js
@@ -1,27 +1,23 @@
 function toWeirdCase(str) {
-  // have variable that is initialized as: isEven = true
-  //   set isEven to true when you hit a space
-  //   flip isEven after modifying each Character
-  // if ifEven is true, do char.toUpperCase
-  //   if false, do char.toLowerCase
-  // return string
   let isEven = true;
   let result = '';
   for (let i = 0; i < str.length; i++) {
     if (str[i] === ' ') {
-      isEven = true;
       result += str[i];
+      isEven = true;
     } else {
-      if (isEven) {
-        result += str[i].toUpperCase();
-        isEven = !isEven;
-      } else {
-        result += str[i].toLowerCase();
-        isEven = !isEven;
-      }
+      result += isEven ? str[i].toUpperCase() : str[i].toLowerCase();
+      isEven = !isEven;
     }
   }
   return result;
 }
 
 console.log(toWeirdCase('Hello there my name is greg'))
+
+//   have variable that is initialized as: isEven = true
+//   set isEven to true when you hit a space
+//   flip isEven after modifying each Character
+//   if ifEven is true, do char.toUpperCase
+//   if false, do char.toLowerCase
+//   return string
