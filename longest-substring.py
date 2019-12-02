4d2c7259137b62029a16f039273c0c5c8af9119c
Greg <gmerrill03@gmail.com>
2019-12-02 14:57:15 -0800

    Refactor solution for longestsubstr problem

--- longest-substring.py
+++ longest-substring.py
@@ -4,28 +4,19 @@ ex2 = 'loddktdji'
 
 def longest_substr(s):
   hash_map = {}
-  substr = s[0]
-  hash_map[substr] = 0
-  temp_max_length = 1
-  max_length = 1
+  substr = ''
+  temp_max_length = 0
+  max_length = 0
   start_idx = 0
-  iterator = 1
   for idx, char in enumerate(s):
-    if idx == 0: continue
-    if char in hash_map:
+    if char in hash_map and hash_map[char] >= start_idx:
       # check if length of substr is longer than max_length
       if temp_max_length > max_length:
         max_length = temp_max_length
-      
-      
       # reset substr
       substr += char
-      substr = substr[(hash_map[char] + iterator):]
-      iterator -= 1
-      # reset hashmap
-      # how to remove the values in the hashmap before the repeated char ???
-      # answer - you dont!, just check if hashmap index is greater? than current
-      start_idx = idx + 1
+      start_idx = hash_map[char] + 1
+      substr = s[start_idx:(idx + 1)]
       hash_map[char] = idx
       # reset temp_max_length
       temp_max_length = len(substr)
@@ -33,7 +24,7 @@ def longest_substr(s):
       substr += char
       temp_max_length += 1
       hash_map[char] = idx
-    print(f'{idx})', char, substr)
+    print(f'{idx})', char, substr, max_length)
   if temp_max_length > max_length:
     max_length = temp_max_length
   return max_length
